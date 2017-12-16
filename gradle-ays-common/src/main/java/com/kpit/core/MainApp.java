9a53450c488c4dbb0258c2ab0444a7e3a6616a83
ayush <ayush@ayush-PC>
2017-12-16 12:35:45 +0530

    gradlesample

---
+++ gradle-ays-common/src/main/java/com/kpit/core/MainApp.java
@@ -0,0 +1,19 @@
+package com.kpit.core;
+
+/**
+ * 
+ *@author ayush Dec 14, 2017
+ */
+public class MainApp {
+
+	/**
+	 * 
+	 */
+	public MainApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
